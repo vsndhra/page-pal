800c6236c499810507dd6538d261b16d73006088
vsndhra <vasundhara13001@gmail.com>
2023-06-19 17:34:34 +0530

    Addded Login and Registration Pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/page-pal', function () {
+    return view('home');
 });
+
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+
